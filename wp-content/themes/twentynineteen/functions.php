9796751432ff17dd1cb85158036ca31c3c954614
Simon Berton <simonberton@gmail.com>
2019-06-19 19:10:54 -0300

    Testimonial post type

--- wp-content/themes/twentynineteen/functions.php
+++ wp-content/themes/twentynineteen/functions.php
@@ -321,3 +321,31 @@ require get_template_directory() . '/inc/template-tags.php';
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
+
+//CREATE THE CUSTOM POST TYPE PARKING
+function my_custom_post_testimonial() {
+    $labels = [
+        'name' => _x( 'Testimonials', 'Testimonials' ),
+        'singular_name' => _x( 'Testimonial', 'Testimonial' ),
+        'add_new' => _x( 'Add New', 'Testimonial' ),
+        'add_new_item' => __( 'Add New Testimonial' ),
+        'edit_item' => __( 'Edit Testimonial' ),
+        'new_item' => __( 'New Testimonial' ),
+        'all_items' => __( 'All Testimonials' ),
+        'view_item' => __( 'View Testimonials' ),
+        'search_items' => __( 'Search Testimonial' ),
+        'not_found' => __( 'No Testimonials found' ),
+        'not_found_in_trash' => __( 'No Testimonials found in the Trash' ),
+        'menu_name' => 'Parkings'
+    ];
+    $args = [
+        'labels' => $labels,
+        'description' => 'Displays Testimonials',
+        'public' => true,
+        'menu_position' => 3,
+        'menu_icon' => 'dashicons-id',
+        'supports' => array( 'title', 'custom-fields' )
+    ];
+    register_post_type( 'testimonial', $args );
+}
+add_action( 'init', 'my_custom_post_testimonial' );
